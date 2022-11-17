97c594fd02d04d07766bc30a25c8b2b57f589c31
Afnan Attar <afnan.attar74@gmail.com>
2022-11-17 18:31:39 +0530

    Project Added

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import '../styles/globals.css'
+import {UberProvider} from '../context/uberContext'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <UberProvider>
+      <Component {...pageProps} />
+    </UberProvider>
+  )
+}
+
+export default MyApp
